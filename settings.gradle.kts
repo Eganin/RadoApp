fa1973f523a847a6d949d3242316f6ef62ce3cf8
Eganin <egorzaharin12@gmail.com>
2023-09-29 19:03:40 +0500

    init project

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Rado"
+include(":androidApp")
+include(":common")
+include(":common:core")
