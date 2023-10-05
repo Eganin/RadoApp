97a0ab18cde0c26971e00b0297ccd897ce1cd075
Eganin <egorzaharin12@gmail.com>
2023-10-05 12:44:20 +0500

    added HttpEngineFactory

---
+++ common/core/src/iosMain/kotlin/ktor/HttpEngineFactory.kt
@@ -0,0 +1,8 @@
+package ktor
+
+import io.ktor.client.engine.*
+import io.ktor.client.engine.darwin.*
+
+internal actual class HttpEngineFactory actual constructor() {
+    actual fun createEngine(): HttpClientEngineFactory<HttpClientEngineConfig> = Darwin
+}
