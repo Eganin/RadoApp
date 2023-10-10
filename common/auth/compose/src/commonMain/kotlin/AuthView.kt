627c22eab35078ea9ece0da59cc7e8ff0e8f6d07
Eganin <egorzaharin12@gmail.com>
2023-10-10 16:45:57 +0500

    added send login button and phone text field

--- common/auth/compose/src/commonMain/kotlin/AuthView.kt
+++ common/auth/compose/src/commonMain/kotlin/AuthView.kt
@@ -1,6 +1,10 @@
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
@@ -11,6 +15,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.unit.toSize
@@ -25,8 +31,24 @@ fun AuthView(
     modifier: Modifier = Modifier,
     eventHandler: (AuthEvent) -> Unit
 ) {
-    Column(modifier = modifier.fillMaxSize().background(color = Theme.colors.primaryBackground).padding(all = 16.dp)) {
+    Column(
+        modifier = modifier.fillMaxSize().background(color = Theme.colors.primaryBackground).padding(all = 16.dp)
+            .verticalScroll(rememberScrollState())
+    ) {
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Text(
+            text = MainRes.string.sign_in_title,
+            fontSize = 32.sp,
+            color = Theme.colors.primaryTextColor,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
+
+        Spacer(modifier = Modifier.height(64.dp))
+
         Text(text = MainRes.string.choose_position, fontSize = 16.sp, color = Theme.colors.primaryTextColor)
+
         Spacer(modifier = Modifier.height(8.dp))
         //Dropdown menu
         Box(modifier = Modifier.fillMaxWidth()) {
@@ -47,6 +69,7 @@ fun AuthView(
                     eventHandler.invoke(AuthEvent.ExposedMenuSizeChanged(value = it.size.toSize()))
                 }
             )
+
             DropdownMenu(
                 modifier = Modifier.width(with(LocalDensity.current) { state.exposedMenuSize.width.toDp() }),
                 expanded = state.exposedMenuIsEnabled,
@@ -63,11 +86,13 @@ fun AuthView(
         }
 
         // input texts region
-        val textFieldsForFullName by mutableStateOf(listOf(
-            Pair(first = state.firstName, second = MainRes.string.name_title),
-            Pair(first = state.secondName, second = MainRes.string.family_title),
-            Pair(first = state.thirdName, second = MainRes.string.patronymic_title)
-        ))
+        val textFieldsForFullName by mutableStateOf(
+            listOf(
+                Pair(first = state.firstName, second = MainRes.string.name_title),
+                Pair(first = state.secondName, second = MainRes.string.family_title),
+                Pair(first = state.thirdName, second = MainRes.string.patronymic_title)
+            )
+        )
 
         textFieldsForFullName.forEach { (value, title) ->
             Spacer(modifier = Modifier.height(8.dp))
@@ -87,31 +112,26 @@ fun AuthView(
                 modifier = Modifier.fillMaxWidth()
             )
         }
-//        Spacer(modifier=Modifier.height(8.dp))
-//
-//        OutlinedTextField(
-//            value = state.firstName,
-//            onValueChange = {eventHandler.invoke(AuthEvent.FirstNameChanged(value=it))},
-//            label = { Text(text = MainRes.string.name_title, color = Theme.colors.primaryTextColor) },
-//            modifier = Modifier.fillMaxWidth()
-//        )
-//
-//        Spacer(modifier=Modifier.height(8.dp))
-//
-//        OutlinedTextField(
-//            value = state.secondName,
-//            onValueChange = {eventHandler.invoke(AuthEvent.SecondNameChanged(value=it))},
-//            label = { Text(text = MainRes.string.family_title, color = Theme.colors.primaryTextColor) },
-//            modifier = Modifier.fillMaxWidth()
-//        )
-//
-//        Spacer(modifier=Modifier.height(8.dp))
-//
-//        OutlinedTextField(
-//            value = state.thirdName,
-//            onValueChange = {eventHandler.invoke(AuthEvent.SecondNameChanged(value=it))},
-//            label = { Text(text = MainRes.string.patronymic_title, color = Theme.colors.primaryTextColor) },
-//            modifier = Modifier.fillMaxWidth()
-//        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        OutlinedTextField(
+            value = state.phone,
+            onValueChange = { eventHandler.invoke(AuthEvent.PhoneChanged(value = it)) },
+            label = { Text(text = MainRes.string.phone_title, color = Theme.colors.primaryTextColor) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone),
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        //register/login button
+        Spacer(modifier = Modifier.height(64.dp))
+
+        Button(
+            onClick = { eventHandler.invoke(AuthEvent.RegisterClick) },
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(size = 16.dp)
+        ) {
+            Text(text = MainRes.string.sign_in_button_title, fontSize = 24.sp, color = Theme.colors.secondaryTextColor)
+        }
     }
 }
