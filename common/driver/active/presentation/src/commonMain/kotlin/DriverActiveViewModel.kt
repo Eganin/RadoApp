70998c76e4bfe39833f3d42cd19c19955e7ff3e6
Eganin <egorzaharin12@gmail.com>
2023-10-13 14:09:30 +0500

    create request for DriverActiveViewModel

--- common/driver/active/presentation/src/commonMain/kotlin/DriverActiveViewModel.kt
+++ common/driver/active/presentation/src/commonMain/kotlin/DriverActiveViewModel.kt
@@ -9,21 +9,20 @@ class DriverActiveViewModel : BaseSharedViewModel<DriverActiveViewState, DriverA
 ) {
 
     private val coroutineScope =
-        CoroutineScope(SupervisorJob() + Dispatchers.Default +
-                CoroutineExceptionHandler { _, throwable ->
-                    log(tag = TAG) { throwable.message ?: "Error" }
-                })
+        CoroutineScope(SupervisorJob() + Dispatchers.Default + CoroutineExceptionHandler { _, throwable ->
+            log(tag = TAG) { throwable.message ?: "Error" }
+        })
 
     private val activeRequestsRepository: ActiveRequestsForDriverRepository = Inject.instance()
 
     init {
-        getActiveRequests()
+        getActiveRequestsByDate()
     }
 
     override fun obtainEvent(viewEvent: DriverActiveEvent) {
         when (viewEvent) {
-            is DriverActiveEvent.SelectedDateChanged -> {}
-            is DriverActiveEvent.CreateRequest -> {}
+            is DriverActiveEvent.CreateRequest -> createRequest()
+            is DriverActiveEvent.SelectedDateChanged -> getActiveRequestsByDate(date = viewEvent.value)
             is DriverActiveEvent.SelectedTypeVehicleChanged -> obtainSelectedTypeVehicleChange(typeVehicle = viewEvent.value)
             is DriverActiveEvent.NumberVehicleChanged -> obtainNumberVehicleChange(numberVehicle = viewEvent.value)
             is DriverActiveEvent.FaultDescriptionChanged -> obtainFaultDescriptionChange(faultDescription = viewEvent.value)
@@ -31,14 +30,33 @@ class DriverActiveViewModel : BaseSharedViewModel<DriverActiveViewState, DriverA
         }
     }
 
-    private fun getActiveRequests() {
+    private fun createRequest() {
         coroutineScope.launch {
-            val activeRequestsForDriverItem = activeRequestsRepository.getRequestsByDate(date = "")
+            val createRequestIdItem = activeRequestsRepository.createRequest(
+                typeVehicle = viewState.selectedVehicleType.nameVehicleType,
+                numberVehicle = viewState.numberVehicle,
+                faultDescription = viewState.faultDescription
+            )
+            if (createRequestIdItem is CreateRequestIdItem.Success) {
+                log(tag = TAG) { "Create request is success" }
+                viewAction = DriverActiveAction.ShowSuccessCreateRequestDialog
+            } else if (createRequestIdItem is CreateRequestIdItem.Error) {
+                log(tag = TAG) { "Create request is failure" }
+                viewAction = DriverActiveAction.ShowErrorSnackBar(message = createRequestIdItem.message)
+            }
+        }
+    }
+
+    private fun getActiveRequestsByDate(date: String = "") {
+        coroutineScope.launch {
+            val activeRequestsForDriverItem = activeRequestsRepository.getRequestsByDate(date = date)
             if (activeRequestsForDriverItem is ActiveRequestsForDriverItem.Success) {
+                log(tag = TAG) { activeRequestsForDriverItem.items.toString() }
                 viewState = viewState.copy(
                     requests = activeRequestsForDriverItem.items
                 )
             } else if (activeRequestsForDriverItem is ActiveRequestsForDriverItem.Error) {
+                log(tag = TAG) { "Active Requests without date is failure" }
                 obtainEvent(viewEvent = DriverActiveEvent.ErrorTextForRequestListChanged(value = activeRequestsForDriverItem.message))
             }
         }
@@ -48,24 +66,28 @@ class DriverActiveViewModel : BaseSharedViewModel<DriverActiveViewState, DriverA
         viewState = viewState.copy(
             errorTextForRequestList = errorMessage
         )
+        log(tag = TAG) { "Error message for requests setup" }
     }
 
     private fun obtainSelectedTypeVehicleChange(typeVehicle: VehicleType) {
         viewState = viewState.copy(
             selectedVehicleType = typeVehicle
         )
+        log(tag = TAG) { "Type vehicle is changed" }
     }
 
     private fun obtainNumberVehicleChange(numberVehicle: String) {
         viewState = viewState.copy(
             numberVehicle = numberVehicle
         )
+        log(tag = TAG) { "Number vehicle is changed" }
     }
 
     private fun obtainFaultDescriptionChange(faultDescription: String) {
         viewState = viewState.copy(
             faultDescription = faultDescription
         )
+        log(tag = TAG) { "Fault description is changed" }
     }
 
     private companion object {
--- common/driver/active/presentation/src/commonMain/kotlin/models/DriverActiveAction.kt
+++ common/driver/active/presentation/src/commonMain/kotlin/models/DriverActiveAction.kt
@@ -2,7 +2,9 @@ package models
 
 sealed class DriverActiveAction {
 
-    data class ShowErrorSnackBar(val message: String): DriverActiveAction()
+    data class ShowErrorSnackBar(val message: String) : DriverActiveAction()
 
-    data object OpenCreateRequestScreen: DriverActiveAction()
+    data object ShowSuccessCreateRequestDialog : DriverActiveAction()
+
+    data object OpenCreateRequestScreen : DriverActiveAction()
 }
