56957d18528bd411b5bfe25da6e2f502ef56f91e
eganin <egorzaharin12@gmail.com>
2023-10-17 15:27:39 +0500

    added pod and cacaopods

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -1,11 +1,11 @@
 import SwiftUI
-import common
+import SharedSDK
 
 struct ContentView: View {
-	let greet = Greeting().greet()
+	//let greet = Greeting().greet()
 
 	var body: some View {
-		Text(greet)
+		Text("Test")
 	}
 }
 
